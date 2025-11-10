8b5f2936fd9742d2cdade76ff0ab2f390b93336f
narriman <narimanamer805@gmail.com>
2025-11-10 20:56:40 +0200

    solve from 63 to 66

---
+++ Thinkful Logic Drills Traffic light.js
@@ -0,0 +1,28 @@
+//Problem:Thinkful Logic Drills Traffic light 
+
+/**
+ * You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, when the input is green, output should be yellow.
+ */
+
+//Solution
+
+function updateLight(current) {
+  //your code here!
+  let lightArray = ['green', 'yellow','red'];
+  if(lightArray.indexOf(current) ===2){
+    return 'green';
+  }else{
+    return lightArray[lightArray.indexOf(current)+1]
+  }
+}
+//Another Solution
+
+function updateLight(current) {
+  //your code here!
+ return current === 'yellow' ? 'red' : current === 'green'? 'yellow' : 'green';
+}
+
